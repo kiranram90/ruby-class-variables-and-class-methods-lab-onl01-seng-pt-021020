78fd06a22243f5d11126b91ce66f73b7ef6d52e8
kiran ram <kram@lions.molloy.edu>
2020-03-03 03:52:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,23 +1,31 @@
 class Song 
   attr_accessor :name, :artist, :genre
   @@count = 0 
-  genres = []
-  artists = []
+  @@genres = []
+  @@artists = []
   
   def initialize(name, artist, genre)
     @@count += 1 
-    genres << @@genre
-    end
+    @artist = artist
+    @genre = genre
+    @@artists << artist
+    @@genrest << genre
+        end
     
     def self.count
       @@count
     end
     
     def self.genres 
-      genres
+      @@genres.uniq
     end
     
     def self.artisits
-      
+      @@artists.uniq
     end
+    
+    def self.genre_count
+      genre_count = Hash.new(0)
+      @@genres.each {|genre| genre_count[genre] +=1}
+      genre_count
 end
