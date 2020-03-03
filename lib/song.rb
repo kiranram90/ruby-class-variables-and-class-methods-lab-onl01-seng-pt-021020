4c2f73af5661d491f85e7433e9662ab6ea7e489a
kiran ram <kram@lions.molloy.edu>
2020-03-03 03:19:57 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,23 @@
+class Song 
+  attr_accessor :name, :artist, :genre
+  @@count = 0 
+  genres = []
+  artists = []
+  
+  def initialize(name, artist, genre)
+    @@count += 1 
+    genres << @@genre
+    end
+    
+    def self.count
+      @@count
+    end
+    
+    def self.genres 
+      genres
+    end
+    
+    def self.artisits
+      
+    end
+end
